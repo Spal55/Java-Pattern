5875ea49cdd98430c3c7dc6fecb8e48a508d1469
Suparno Pal <pal.suparno2001@gmail.com>
2021-07-11 02:29:54 +0530

    Add files via upload

--- pattern.java
+++ pattern.java
@@ -1,33 +1,45 @@
-import java.util.*;
-
+import java.util.Scanner;
 public class pattern
 {
     public static void main(String[] args)
     {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the height of the pattern: ");
-        int height=sc.nextInt();
-        int h=height-1;
-        System.out.println("\nThis the pattern:\n\n");
+        int n = sc.nextInt();
+        int i,j,k;
 
-        for (int i=0;i<=h;i++)
+        for(i =1; i <= n; i++)
         {
-            for (int j=0;j<=i;j++)
-                System.out.print("*");
-            for (int s=0;s<=2*(h-i);s++)
-                System.out.print(" ");
-            for (int j=i;j>=0;j--)
-                System.out.print("*");
+
+            for(j =1; j <= n; j++)
+            {
+                if(j <= i)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            for(j = n-1; j >= 1; j--)
+            {
+                if(j <= i)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+
             System.out.println();
         }
-        for (int i=h;i>=0;i--)
+        for(i =1; i <= n; i++)
         {
-            for (int j=0;j<=i;j++)
-                System.out.print(" ");
-            for (int k=i;k<=h;k++)
-                System.out.print("*"+" ");
+
+            for (j = n; j > i; j--)
+                System.out.print("  ");
+
+            for (k = 1; k < 2*i; k++)
+                System.out.print("* ");
+
             System.out.println();
         }
+
+
     }
 }
